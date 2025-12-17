93513574234b215bb904d9ec84630db0dd030dc5
SasaniSWidanapathirana <2019is094@stu.ucsc.lk>
2025-12-17 11:47:58 +0530

    admin.css, sidenav.css,topbar.css,admin_panel.php and sidenav.php has been modified

--- views/components/sidenav.php
+++ views/components/sidenav.php
@@ -4,24 +4,27 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/htdocs/public/css/sidenav.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=person" />
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=call" />
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@24,300,0,0&icon_names=event_note" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded" />
 
 </head>
 <body>
     <div class="sidenav">
+
+        <div class="logo">
         <a href="../../index.php">
             <img src="/htdocs/public/images/logo2.png">   
         </a>
-        <a href='../admin/admin_panel.php'>
+        </div>
+
+        <div class="menu">
+        <a href='../admin/admin_panel.php' class="active">
             <span class="material-symbols-rounded">
-            person
+            event
             </span>Current Events</a>
 
         <a href='../admin/past_event.php'>
             <span class="material-symbols-rounded">
-            person
+            event_available
             </span>Past Events</a>
 
         <a href='../admin/members.php'>
@@ -33,6 +36,8 @@
             <span class="material-symbols-rounded">
             call
             </span>Contact</a>
+        </div>
+        
     </div>
 </body>
 </html>

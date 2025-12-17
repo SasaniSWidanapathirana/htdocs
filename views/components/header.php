e7dc4dba7bd3b9acfce764554075b04566e1f013
SasaniSWidanapathirana <2019is094@stu.ucsc.lk>
2025-12-17 12:07:57 +0530

    removed unused coding parts

--- views/components/header.php
+++ views/components/header.php
@@ -5,7 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>header</title>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
-    <!-- <link rel="stylesheet" href="../../public/css/header.css"> -->
     <link rel="stylesheet" href="/htdocs/public/css/header.css">
 
 
@@ -23,7 +22,6 @@
         <a href="#" class="active">Home</a>
         <a href='/views/common/about.html'>About Us</a>
         <a href="#">Contact Us</a>
-        <!-- <a href='../../index.php'>Login</a>  -->
         <button onclick="location.href='/views/common/login.html'">Login</button>
     </nav>
 </div>

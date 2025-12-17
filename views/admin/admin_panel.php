44f468c0433df32f32c8d8db630919bb0390756f
SasaniSWidanapathirana <sasanisamanga@gmail.com>
2025-12-17 21:52:58 +0530

    table changed and bg color changed

--- views/admin/admin_panel.php
+++ views/admin/admin_panel.php
@@ -19,7 +19,6 @@ $events = $eventObj->getAllEvents();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="/htdocs/public/js/addEvent.js"></script>
-    <!-- <link rel="stylesheet" href="../../public/css/style.css"> -->
     <link rel="stylesheet" href="../../public/css/admin.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded" />
     <title>Admin Panel</title>
@@ -81,27 +80,36 @@ $events = $eventObj->getAllEvents();
                 </div>
             </div>
 
-            <table class="event-table">
-                <tr>
-                    <th>ID</th>
-                    <th>Title</th>
-                    <th>Description</th>
-                    <th>Date & Time</th>
-                    <th>Location</th>
-                    <th>Expected Count</th>
-                </tr>
-
-                <?php while ($row = $events->fetch(PDO::FETCH_ASSOC)) : ?>
+            <div class="event-table-wrapper">
+                <table class="event-table">
                     <tr>
-                        <td><?= htmlspecialchars($row['event_id']); ?></td>
-                        <td><?= htmlspecialchars($row['title']); ?></td>
-                        <td><?= htmlspecialchars($row['description']); ?></td>
-                        <td><?= htmlspecialchars($row['date_time']); ?></td>
-                        <td><?= htmlspecialchars($row['location']); ?></td>
-                        <td><?= htmlspecialchars($row['exp_cnt']); ?></td>
+                        <th>Title</th>
+                        <th>Description</th>
+                        <th>Date & Time</th>
+                        <th>Location</th>
+                        <th>Expected Count</th>
+                        <th>Actions</th>
                     </tr>
-                <?php endwhile; ?>
-            </table>
+
+                    <?php while ($row = $events->fetch(PDO::FETCH_ASSOC)) : ?>
+                        <tr>
+                            <td><?= htmlspecialchars($row['title']); ?></td>
+                            <td><?= htmlspecialchars($row['description']); ?></td>
+                            <td><?= htmlspecialchars($row['date_time']); ?></td>
+                            <td><?= htmlspecialchars($row['location']); ?></td>
+                            <td><?= htmlspecialchars($row['exp_cnt']); ?></td>
+                            <td class="action-cell">
+                                <a class="action-btn edit" href="edit_event.php?id=<?= urlencode($row['event_id']); ?>" title="Edit">
+                                    <span class="material-symbols-rounded">edit</span>
+                                </a>
+                                <a class="action-btn delete" href="delete_event.php?id=<?= urlencode($row['event_id']); ?>" title="Delete" onclick="return confirm('Delete this event?');">
+                                    <span class="material-symbols-rounded">delete</span>
+                                </a>
+                            </td>
+                        </tr>
+                    <?php endwhile; ?>
+                </table>
+            </div>
 
         </main>
     </div>
